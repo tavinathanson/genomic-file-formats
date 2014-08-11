a5ac57bfb94e14b76e1bc22385d510090b146b8a
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-08-11 17:06:20 -0400

    Run MAF describe on a whole directory

--- maf_describe.py
+++ maf_describe.py
@@ -5,34 +5,38 @@ Read a MAF file, and describe it.
 import pandas as pd
 import argparse
 import sys
+import os
 
 MAF_COLUMN_NAMES = [
-    'Hugo_Symbol',
-    'Entrez_Gene_Id',
-    'Center',
-    'NCBI_Build',
-    'Chromosome',
-    'Start_Position',
-    'End_Position',
-    'Strand',
-    'Variant_Classification',
-    'Variant_Type',
-    'Reference_Allele',
-    'Tumor_Seq_Allele1',
-    'Tumor_Seq_Allele2',
-    'dbSNP_RS',
-    'dbSNP_Val_Status',
-    'Tumor_Sample_Barcode',
-    'Matched_Norm_Sample_Barcode',
-    'Match_Norm_Seq_Allele1',
-    'Match_Norm_Seq_Allele2'
+    "Hugo_Symbol",
+    "Entrez_Gene_Id",
+    "Center",
+    "NCBI_Build",
+    "Chromosome",
+    "Start_Position",
+    "End_Position",
+    "Strand",
+    "Variant_Classification",
+    "Variant_Type",
+    "Reference_Allele",
+    "Tumor_Seq_Allele1",
+    "Tumor_Seq_Allele2",
+    "dbSNP_RS",
+    "dbSNP_Val_Status",
+    "Tumor_Sample_Barcode",
+    "Matched_Norm_Sample_Barcode",
+    "Match_Norm_Seq_Allele1",
+    "Match_Norm_Seq_Allele2"
 ]
 
 def run(args):
-    maf_df = get_maf_df(args)
-    allele_ref = maf_df['Reference_Allele']
-    allele_1 = maf_df['Tumor_Seq_Allele1']
-    allele_2 = maf_df['Tumor_Seq_Allele2']
+    if args.maf is not None:
+        maf_df = get_maf_df(args.maf)
+    else:
+        maf_df = get_maf_dir_df(args.maf_dir)
+    allele_ref = maf_df["Reference_Allele"]
+    allele_1 = maf_df["Tumor_Seq_Allele1"]
+    allele_2 = maf_df["Tumor_Seq_Allele2"]
     allele_1_vars = maf_df[(allele_ref == allele_2) & (allele_ref != allele_1)]
     allele_2_vars = maf_df[(allele_ref == allele_1) & (allele_ref != allele_2)]
     allele_both_vars = maf_df[(allele_ref != allele_1) & (allele_ref != allele_2)]
@@ -43,9 +47,14 @@ def run(args):
     print("Both alleles are different from the ref: %d" % len(allele_both_vars))
     print("Both alleles are the same as the ref: %d" % len(allele_none_vars))
 
-def get_maf_df(args):
-    filename = args.filename[0]
-    df = pd.read_csv(filename, sep='\t')
+def get_maf_dir_df(dirname):
+    dfs = [get_maf_df(os.path.join(dirname, filename)) for filename \
+            in os.listdir(dirname) if ".maf" in filename]
+    df = pd.concat(dfs)
+    return df
+
+def get_maf_df(filename):
+    df = pd.read_csv(filename, sep="\t")
     with open(filename) as fd:
         lines_to_skip = 0
         while next(fd).startswith('#'):
@@ -59,8 +68,8 @@ def get_maf_df(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage=__doc__)
-    parser.add_argument("filename",
-            metavar="filename", nargs=1,
-            help="a MAF file")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--maf", help="a MAF file")
+    group.add_argument("--maf-dir", help="a MAF dir")
     args = parser.parse_args()
     run(args)
